2a4b4b4b8dcd47856c6cd93040fd374f84f27766
AntoineThorel <91877195+AntoineThorel@users.noreply.github.com>
2021-11-07 23:27:00 +0100

    Update tp02_ex1.py

--- tp02_ex1.py
+++ tp02_ex1.py
@@ -11,13 +11,22 @@ chaque chiffre entier compris entre deux valeurs demandées à l'utilisateur.
 """
 
 ###Déclaration et Initialisation des variables
-
-
-
+chiffre_1: int = None
+chiffre_2: int = None
+nb_ops: int = 0
+var: int = None
+chiffre_1 = int(input("Choisissez un chiffre de départ "))
+chiffre_2 = int(input("Choisissez un chiffre de fin "))
 ### Séquence d'opération
-
-        
-            
-
-
-
+for nb in range(chiffre_1, chiffre_2 + 1):
+    var = nb
+    while var != 0:
+        if var % 3 == 0:
+            var += 4
+        elif var % 4 == 0:
+            var = var / 2
+        else:
+            var -= 1
+        nb_ops += 1
+    print (str(nb) + " -> " + str(nb_ops))
+    nb_ops = 0
